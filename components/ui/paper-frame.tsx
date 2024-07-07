28062e238e2d2d7eecdadaed5e05cd857d9f9ab9
Mohit Sharma <mohit1.sharma@cars24.com>
2024-07-07 17:15:56 +0530

    feat: add answer in the print frame

--- components/ui/paper-frame.tsx
+++ components/ui/paper-frame.tsx
@@ -19,17 +19,41 @@ const PaperFrame = ({
   ...rest
 }: Props) => {
   return (
-    <ol
-      className={cn("border p-4 ", twoColumn && "columns-2", className)}
-      {...rest}
-    >
-      {questions?.map((q, index) => (
-        <label key={index} className="flex items-start  rounded p-2">
-          <span className="pt-[10px] px-2">{index + 1}.</span>
-          <MemoizedMathpixMarkdown text={q.text ?? ""} />
-        </label>
-      ))}
-    </ol>
+    <div>
+      <h1 className="font-medium w-full pb-4 text-center">QUESTIONS</h1>
+      <ol
+        className={cn("border p-4 mt-4", twoColumn && "columns-2", className)}
+        {...rest}
+      >
+        {questions?.map((q, index) => (
+          <label key={index} className="flex items-start  rounded p-2">
+            <span className="pt-[10px] px-2">{index + 1}.</span>
+            <MemoizedMathpixMarkdown text={q.text ?? ""} />
+          </label>
+        ))}
+      </ol>
+      <h1
+        className="font-medium w-full mt-8 pb-4 text-center break-before-page"
+      >
+        ANSWERS
+      </h1>
+      <ol
+        className={cn(
+          "border p-4 flex flex-wrap [&_#preview]:!px-0 [&_#preview]:!max-w-[300px] md:[&_#preview]:!max-w-full "
+        )}
+        {...rest}
+      >
+        {questions?.map((q, index) => (
+          <label
+            key={index}
+            className="flex items-baseline justify-center rounded p-2"
+          >
+            <span className="pt-[10px] px-2">{index + 1}.</span>
+            <MemoizedMathpixMarkdown text={q.ans ?? ""} />
+          </label>
+        ))}
+      </ol>
+    </div>
   );
 };
 
