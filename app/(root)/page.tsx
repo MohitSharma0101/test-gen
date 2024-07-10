1d827cfae7fca351d46651681d31d27e0a62c1d1
Mohit Sharma <mohit1.sharma@cars24.com>
2024-07-11 00:04:24 +0530

    fix: undo updateUsage comment

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -105,7 +105,7 @@ export default function Home() {
                   className="px-6"
                   onClick={() => {
                     startTransition(() => {
-                      // updateUsage(selectedQuestions);
+                      updateUsage(selectedQuestions);
                     });
                   }}
                 >
