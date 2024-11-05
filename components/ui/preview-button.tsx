4853b3686947fdac7430c4b164ff96fb5c424890
Mohit Sharma <mohit1.sharma@cars24.com>
2024-11-05 19:51:37 +0530

    optimize preview button and print comp

--- components/ui/preview-button.tsx
+++ components/ui/preview-button.tsx
@@ -1,10 +1,14 @@
-import React, { startTransition, useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useState,
+  useTransition,
+} from "react";
 import { Sheet, SheetContent, SheetTrigger } from "./sheet";
 import { Button } from "./button";
 import { Print, PrintContent, PrintTrigger } from "./print";
 import PaperFrame from "../frames/paper-frame";
 import type { TQuestion } from "@/models/Question";
-import { Columns2Icon } from "lucide-react";
+import { Columns2Icon, ListRestartIcon, Loader2Icon } from "lucide-react";
 import MCQAnsFrame from "../frames/mcq-ans-frame";
 import TeachersCopiesFrame from "../frames/teachers-copies-frame";
 import CopyQuestionsMd from "./copy--questions-md";
@@ -23,6 +27,8 @@ const PreviewButton = ({
   className,
 }: Props) => {
   const [twoColumn, setTwoColumn] = useState(defaultTwoColumn);
+  const [loadPreview, setLoadPreview] = useState(false);
+  const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
     if (defaultTwoColumn != twoColumn) {
@@ -43,51 +49,73 @@ const PreviewButton = ({
       </SheetTrigger>
       <SheetContent
         side={"bottom"}
-        className="max-h-[80svh] m-auto rounded max-w-screen-lg scrollbar-hide overflow-scroll"
+        className="max-h-[90svh] m-auto rounded max-w-screen-lg scrollbar-hide overflow-scroll"
       >
-        <Print>
-          <div className="flex gap-2">
+        <div className="flex gap-2 bg-white w-full mr-5 z-10">
+          <Print>
             <PrintTrigger
               className="px-6"
               onClick={() => {
                 startTransition(() => {
-                  onPrint?.();
+                  // onPrint?.();
                 });
               }}
               disabled={questions.length === 0}
             >
               Print
             </PrintTrigger>
-            <Button
-              variant={twoColumn ? "default" : "outline"}
-              onClick={() => {
-                setTwoColumn(!twoColumn);
-              }}
-            >
-              <Columns2Icon className="w-4 h-4" />
-            </Button>
-            <Print>
-              <PrintTrigger>Print MCQ Answer</PrintTrigger>
-              <PrintContent>
-                <MCQAnsFrame questions={questions} />
-              </PrintContent>
-            </Print>
-            <Print>
-              <PrintTrigger>Print Teachers Copy</PrintTrigger>
-              <PrintContent>
-                <TeachersCopiesFrame
-                  questions={questions}
-                  twoColumn={twoColumn}
-                />
-              </PrintContent>
-            </Print>
-            <CopyQuestionsMd questions={questions} />
-          </div>
+            <PrintContent>
+              <PaperFrame questions={questions} twoColumn={twoColumn} />
+            </PrintContent>
+          </Print>
+          <Print>
+            <PrintTrigger>Print MCQ Answer</PrintTrigger>
+            <PrintContent>
+              <MCQAnsFrame questions={questions} />
+            </PrintContent>
+          </Print>
+          <Print>
+            <PrintTrigger>Print Teachers Copy</PrintTrigger>
+            <PrintContent>
+              <TeachersCopiesFrame
+                questions={questions}
+                twoColumn={twoColumn}
+              />
+            </PrintContent>
+          </Print>
+          <CopyQuestionsMd questions={questions} />
 
-          <PrintContent className="block">
-            <PaperFrame questions={questions} twoColumn={twoColumn} />
-          </PrintContent>
-        </Print>
+          <Button
+            variant={twoColumn ? "default" : "outline"}
+            onClick={() => {
+              setTwoColumn(!twoColumn);
+            }}
+            className="ml-auto mr-4"
+          >
+            <Columns2Icon className="w-4 h-4" />
+          </Button>
+        </div>
+        {loadPreview ? (
+          <PaperFrame questions={questions} twoColumn={twoColumn} />
+        ) : (
+          <Button
+            disabled={isPending}
+            variant={"outline"}
+            className=" w-full flex items-center justify-center h-[600px] text-base border border-dashed border-slate-400 bg-slate-50 rounded-xl my-6"
+            onClick={() => {
+              startTransition(() => {
+                setLoadPreview(true);
+              });
+            }}
+          >
+            {isPending ? (
+              <Loader2Icon className="w-5 h-5 mr-2 animate-spin" />
+            ) : (
+              <ListRestartIcon className="w-5 h-5 mr-2" />
+            )}
+            Load Preview
+          </Button>
+        )}
       </SheetContent>
     </Sheet>
   );
--- components/ui/print.tsx
+++ components/ui/print.tsx
@@ -4,11 +4,15 @@ import React, {
   ReactNode,
   RefObject,
   useContext,
+  useEffect,
   useRef,
+  useState,
+  useTransition,
 } from "react";
 import { useReactToPrint } from "react-to-print";
 import { Button } from "./button";
 import { cn } from "@/lib/utils";
+import { Loader2Icon } from "lucide-react";
 
 type Props = {
   children: ReactNode;
@@ -17,6 +21,7 @@ type Props = {
 export const PrintContext = createContext<{
   printRef?: RefObject<HTMLDivElement>;
   handlePrint?: ReturnType<typeof useReactToPrint>;
+  loadContent?: boolean;
 }>({});
 
 export const usePrint = () => {
@@ -27,23 +32,30 @@ export const usePrint = () => {
   return context;
 };
 
-export const PrintProvider = ({ children }: Props) => {
+export const Print = ({ children }: Props) => {
   const printRef = useRef<HTMLDivElement>(null);
+  const [loadContent, setLoadContent] = useState(false);
   const handlePrint = useReactToPrint({
     content: () => printRef.current,
   });
 
+  useEffect(() => {
+    if (loadContent) {
+      handlePrint();
+    }
+  }, [loadContent]);
+
   return (
     <PrintContext.Provider
-      value={{ printRef: printRef, handlePrint: handlePrint }}
+      value={{
+        printRef: printRef,
+        handlePrint: () => setLoadContent(true),
+        loadContent: loadContent,
+      }}
     >
       {children}
     </PrintContext.Provider>
   );
-};
-
-export const Print = ({ children }: Props) => {
-  return <PrintProvider>{children}</PrintProvider>;
 };
 
 export const PrintTrigger = ({
@@ -52,14 +64,19 @@ export const PrintTrigger = ({
   ...rest
 }: Props & ButtonHTMLAttributes<HTMLButtonElement>) => {
   const { handlePrint } = usePrint();
+  const [isPending, startTransition] = useTransition();
   return (
     <Button
       {...rest}
+      disabled={isPending}
       onClick={(e) => {
-        handlePrint?.();
-        onClick?.(e);
+        startTransition(() => {
+          handlePrint?.();
+          onClick?.(e);
+        });
       }}
     >
+      {isPending && <Loader2Icon className="w-4 h-4 mr-2 animate-spin" />}
       {children}
     </Button>
   );
@@ -70,10 +87,12 @@ export const PrintContent = ({
   children,
   ...rest
 }: JSX.IntrinsicElements["div"]) => {
-  const { printRef } = usePrint();
-  return (
-    <div className={cn("hidden", className)} {...rest}>
-      <div ref={printRef}>{children}</div>
-    </div>
-  );
+  const { printRef, loadContent } = usePrint();
+
+  if (loadContent)
+    return (
+      <div className={cn("hidden", className)} {...rest}>
+        <div ref={printRef}>{children}</div>
+      </div>
+    );
 };
